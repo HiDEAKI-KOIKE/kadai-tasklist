8f6defcf8314ee4c04569bd4e50255cd39c59f2c
HiDEAKI.KOIKE <canilpan779@gmail.com>
2021-03-31 07:05:01 +0000

    task-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+    
     def index
-        @tasks = Task.all
+        #@tasks = Task.all
+        if logged_in?
+            @task = current_user.tasks.build
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
     
     
@@ -14,12 +21,14 @@ class TasksController < ApplicationController
     end 
     
     def create 
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
+        #@task = Task.new(task_params)
         
         if @task.save
             flash[:succes] = 'Task　が正常に登録されました'
             redirect_to @task
         else
+            @task = current_user.task.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'Taskが登録されませんでした'
             render :new
         end
@@ -43,7 +52,6 @@ class TasksController < ApplicationController
     
     def destroy
         @task.destroy
-        
         flash[:succes] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end 
@@ -59,6 +67,13 @@ class TasksController < ApplicationController
 
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to login_path
+        end
     end
     
 end
