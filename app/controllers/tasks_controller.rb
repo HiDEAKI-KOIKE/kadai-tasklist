0ba2c94d0e931a407c587940f0a2449c55cdb47a
HiDEAKI.KOIKE <canilpan779@gmail.com>
2021-04-01 10:26:57 +0000

    tasklist-login5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
     def correct_user
         @task = current_user.tasks.find_by(id: params[:id])
         unless @task
-            redirect_to login_path
+            redirect_to root_path
         end
     end
     
